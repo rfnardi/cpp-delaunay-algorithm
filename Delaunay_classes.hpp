47b83c77a944ba22ebb22fa9290bc904996994c8
Rafael Nardi <nardi.rafael@gmail.com>
2020-03-26 20:30:26 -0300

    updating arquivo de classes: changing to hpp

---
+++ Delaunay_classes.hpp
@@ -0,0 +1,28 @@
+class quadrilatero_vizinhos {
+private:
+  /* data */
+
+public:
+  quadrilatero_vizinhos (array A[3]; array B[3]; array C[3]; array D[3]);
+  virtual ~quadrilatero_vizinhos ();
+};
+
+class catalogo_vizinhos {
+private:
+  /* data */
+
+public:
+  catalogo_vizinhos (array P; std::vector<float> ptos_vizinhos;);
+  virtual ~catalogo_vizinhos ();
+};
+
+
+
+class triangulo_Delaunay {
+private:
+  /* data */
+
+public:
+  triangulo_Delaunay (arguments);
+  virtual ~triangulo_Delaunay ();
+};
