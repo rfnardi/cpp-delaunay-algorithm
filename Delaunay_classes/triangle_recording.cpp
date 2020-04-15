eb48fc744b5c2b921dc6915687332c067b7705b6
Rafael Nardi <nardi.rafael@gmail.com>
2020-04-15 13:01:39 -0300

    criando função que determina orientação correta de um triangulo de Delaunay

--- Delaunay_classes/triangle_recording.cpp
+++ Delaunay_classes/triangle_recording.cpp
@@ -1,75 +1,99 @@
-namespace del {
-
-class triangle_Delaunay 
+namespace del 
 {
-private:
-    /* data */
-public:
-    triangle_Delaunay(/* args */);
-    ~triangle_Delaunay();
 
-    del::point A; 
-    del::point B;
-    del::point C;
-
-    std::vector< del::triangle_Delaunay> Triangulacao; //armazena todos os triangulos de Delaunay
-
-    bool contem (point P)
-    {
-        if (P==A || P==B || P==C)
+        class triangle_Delaunay 
         {
-            return true;
+        private:
+            /* data */
+        public:
+            triangle_Delaunay(/* args */);
+            ~triangle_Delaunay();
+
+            del::point A; 
+            del::point B;
+            del::point C;
+
+            std::vector< del::triangle_Delaunay> Triangulation; //Speichert alle Delaunay Dreiecke
+
+            bool contem (point P)
+            {
+                if (P==A || P==B || P==C)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }       
+            }
+
+            bool edge_sharing (triangle_Delaunay T)
+            {
+                if (    (contem(T.A)&&contem(T.B))
+                        ||(contem(T.A)&&contem(T.C))
+                        ||(contem(T.B)&&contem(T.C))
+                    )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+                
+            }
+            void swap () //Austauscht die Orientierung einer Kante 
+            {
+                del::point P = A;
+                A = B;
+                B = P;
+            }
+            
+            bool operator == (triangle_Delaunay T) //Gleichwertigkeit zwischen zwei Delaunay Dreiecke 
+            {
+
+                if (   ((A == T.A) && (B == T.B) && (C == T.C))
+                    || ((A == T.B) && (B == T.C) && (C == T.A))
+                    || ((A == T.C) && (B == T.A) && (C == T.B))
+                    )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            bool same_curl (triangle_Delaunay T ) //ACHTUNG1! T muss schon ein orientierte Dreieck sein 
+            {                                       //um diese Funktion richtig funktioniert.
+                if ( (A==T.B)&&(B == T.A)           //ACHTUNG2: T muss auch ein Dreieck sein 
+                    ||(A==T.A)&&(B == T.C)         //der mit der anderer Dreieck ein gemeine Kante hat.
+                    ||(A==T.C)&&(B == T.B)
+                    )
+                {
+                    return true;
+                }
+                else
+                {
+                    swap();
+                    return true;
+                }
+                
+            }
+
+
+
+        };
+
+        triangle_Delaunay::triangle_Delaunay(/* args */)
+        {
         }
-        else
-        {
-            return false;
-        }       
-    }
 
-    bool edge_sharing (triangle_Delaunay T)
-    {
-        if (    (contem(T.A)&&contem(T.B))
-                ||(contem(T.A)&&contem(T.C))
-                ||(contem(T.B)&&contem(T.C))
-            )
+        triangle_Delaunay::~triangle_Delaunay()
         {
-            return true;
         }
-        else
-        {
-            return false;
-        }
-        
-        
-    }
-    void swap ( A , B) //troca a orientação de uma aresta 
-    {
-        del::point P = A;
-        A = B;
-        B = P;
-    }
-    bool operator == (triangle_Delaunay T) //equivalencia entre triangulos de Delaunay
-    if (   ((A == T.A) && (B == T.B) && (C == T.C))
-        || ((A == T.B) && (B == T.C) && (C == T.A))
-        || ((A == T.C) && (B == T.A) && (C == T.B))
-        )
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-
-};
-
-triangle_Delaunay::triangle_Delaunay(/* args */)
-{
-}
-
-triangle_Delaunay::~triangle_Delaunay()
-{
-}
 
 
 
