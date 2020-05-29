1619c086850b687de09ecee7cbdb1d0f289640c2
Rafael Nardi <nardi@pop-os.localdomain>
2020-05-28 22:06:27 -0300

    Exibindo dimensões das cartas no console e modificando tomada de medidas do sólido com inicialização das variáveis que guardam os extremos do sólido com o primeiro ponto do arquivo

--- Delaunay_classes/Atlas_builder.cpp
+++ Delaunay_classes/Atlas_builder.cpp
@@ -18,7 +18,7 @@ int sum_sizes(int I, int* index_vec_ptr)
 }
 
 
-int main (int argc, char* argv[])
+int main (int argc, char* argv[40])
 {
 
   int* ppchart;
@@ -70,40 +70,46 @@ int main (int argc, char* argv[])
 			x =0.0;
 			y =0.0;
 			z =0.0;
-			x_min =0.0;
-			y_min =0.0;
-			z_min =0.0;
-			x_max =0.0;
-			y_max =0.0;
-			z_max =0.0;
+
 			do
 		  {
 				currentLine++;
 				lineIterator = fscanf( file, "%E%E%E \n", &x, &y, &z );
-						if(x<x_min)
-		        {
-		            x_min = x;
-		        }
-		        if(x>x_max)
-		        {
-		            x_max = x;
-		        }
-		        if(y<y_min)
-		        {
-		            y_min = y;
-		        }
-		        if(y>y_max)
-		        {
-		            y_max = y;
-		        }
-		        if(z<z_min)
-		        {
-		            z_min = z;
-		        }
-		        if(z>z_max)
-		        {
-		            z_max = z;
-		        }
+
+				if (currentLine ==1 )
+				{
+					x_min = x;
+					x_max = x;
+					y_min = y;
+					y_max = y;
+					z_min = z;
+					z_max = z;
+				}
+
+				if(x<x_min)
+        {
+            x_min = x;
+        }
+        if(x>x_max)
+        {
+            x_max = x;
+        }
+        if(y<y_min)
+        {
+            y_min = y;
+        }
+        if(y>y_max)
+        {
+            y_max = y;
+        }
+        if(z<z_min)
+        {
+            z_min = z;
+        }
+        if(z>z_max)
+        {
+            z_max = z;
+        }
 			} while ( lineIterator != EOF );
 		}
     std::cout<<"Dimensões do sólido: \n";
@@ -116,7 +122,9 @@ int main (int argc, char* argv[])
 
     std::cout<<"Quantidade total de pontos: "<< Total_de_Pontos << '\n';
 
-    int number_of_charts = (int) (((float)Total_de_Pontos) /((float)Points_per_Chart));
+    int number_of_charts;
+		number_of_charts = (int) (((float)Total_de_Pontos) /((float)Points_per_Chart));
+		number_of_charts++;
     std::cout << "Total de cartas: " << number_of_charts <<'\n';
     float chart_depth = (z_max - z_min)/ ((float) number_of_charts); //futuramente considerar fatiamento ao longo de outros eixos de acordo com as proporções do sólido
     std::cout << "Espessura da carta ao longo de z: " << chart_depth << '\n';
@@ -149,6 +157,7 @@ int main (int argc, char* argv[])
 		float* Atlas = (float*) malloc (sizeof(float)*number_of_charts*Points_per_Chart*3);
 
 		int Index_Vector[number_of_charts];
+		//char* Oversized_Charts = (char*) malloc (number_of_charts*sizeof(char));
 		for (size_t i = 0; i < number_of_charts; i++) {
 			Index_Vector[i]=0;
 		}
@@ -167,6 +176,14 @@ int main (int argc, char* argv[])
 
 						Index_Vector[chart_index]++;
 				}
+				if ((chart_index == number_of_charts -1) && (z[i]<=(z_max - chart_index*chart_depth)) && z[i]>(z_max - (chart_index + 1)*chart_depth) && Index_Vector[chart_index]<Points_per_Chart)
+				{
+						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 0), x+i, sizeof(float));
+						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 1), y+i, sizeof(float));
+						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 2), z+i, sizeof(float));
+
+						Index_Vector[chart_index]++;
+				}
 			}
 		}
 
@@ -174,16 +191,15 @@ int main (int argc, char* argv[])
     std::cout << "Particionamento dos pontos concluído com sucesso." << '\n';
 
 		//Mostrando conteúdo das cartas no console:
-		int this_chart_in_particular = 5;
+		int this_chart_in_particular = 0;
 		bool show_points = false;
 		for (size_t chart_index = 0; chart_index < number_of_charts; chart_index++)
     {
-			std::cout << "Quantidade de pontos na carta " << chart_index << ": "<< Index_Vector[chart_index] <<'\n';
 			if (show_points)
 			{
 				if(chart_index ==this_chart_in_particular) //mostra pontos da carta de número 'this_chart_in_particular' se show_points for true
 		    {
-		      std::cout << "Pontos da carta " << chart_index << ":" << '\n';
+		      std::cout << "Pontos da carta " << chart_index << ": " << '\n';
 		      for (size_t j = 0; j < Index_Vector[chart_index]; j++)
 		      {
 		        std::cout << "x = " << *(Atlas + chart_index*Points_per_Chart*3 + 3*j + 0) <<
@@ -192,15 +208,20 @@ int main (int argc, char* argv[])
 		      }
 				}
 			}
+			std::cout << "Quantidade de pontos na carta " << chart_index << ": "<< Index_Vector[chart_index] << " ;    Dimensões:  z max = " << (z_max - chart_index*chart_depth) << " , z min = " << (z_max - (chart_index + 1)*chart_depth) << '\n';
     }
 
-
+		int sum;
+		sum=0;
+		for (size_t i = 0; i < number_of_charts; i++) {sum = sum + Index_Vector[i];}
+		std::cout << "Confirmando total de pontos: " << sum <<'\n';
 
 
 		free(x);
 		free(y);
 		free(z);
 		free(Atlas);
+		//free(Oversized_Charts);
 
 
 return 0;
