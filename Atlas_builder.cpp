8f6f15abe78c799f6a84392ab207961bbb8daff2
Rafael Nardi <nardi@pop-os.localdomain>
2020-05-30 03:26:15 -0300

    Reorganização do Atlas concluída. Agora cada carta consiste de um array completamente ocupado dentro do Atlas. Todos os pontos do arquivo estão reorganizados dentro do atlas. A partir daqui trabalhar na sub-divisão das cartas cuja quantidade de pontos excede limite. Usar para isso os arrays de densidade linear de pontos definidos sob comentário no fim do arquivo Atlas_builder.cpp .

--- Atlas_builder.cpp
+++ Atlas_builder.cpp
@@ -6,13 +6,18 @@
 #include <cstring>
 //#include <bits/stdc++.h>
 
+int sum;
+
+//Useful function for array of arrays with different sizes each:
 int sum_sizes(int I, int* index_vec_ptr)
 {
-	int sum;
 	sum = 0;
-	for (size_t i = I; i > 0; i--)
+	if (I>0)
 	{
-		sum = sum + *(index_vec_ptr + sizeof(int)*I);
+		for (size_t i = 0; i < I; i++)
+		{
+			sum = sum + (int) index_vec_ptr[i];
+		}
 	}
 	return sum;
 }
@@ -63,7 +68,6 @@ int main (int argc, char* argv[40])
 
 	//tomando dimensões do sólido para efetuar fatiamento:
   float x_min, x_max, y_min, y_max, z_min, z_max;
-
 	currentLine =0;
 		{
 			float x, y, z;
@@ -86,39 +90,21 @@ int main (int argc, char* argv[40])
 					z_max = z;
 				}
 
-				if(x<x_min)
-        {
-            x_min = x;
-        }
-        if(x>x_max)
-        {
-            x_max = x;
-        }
-        if(y<y_min)
-        {
-            y_min = y;
-        }
-        if(y>y_max)
-        {
-            y_max = y;
-        }
-        if(z<z_min)
-        {
-            z_min = z;
-        }
-        if(z>z_max)
-        {
-            z_max = z;
-        }
+				if(x<x_min){x_min = x;}
+        if(x>x_max){x_max = x;}
+        if(y<y_min){y_min = y;}
+        if(y>y_max){y_max = y;}
+        if(z<z_min){z_min = z;}
+        if(z>z_max){z_max = z;}
+
 			} while ( lineIterator != EOF );
 		}
+		Total_de_Pontos = currentLine;
     std::cout<<"Dimensões do sólido: \n";
-
     std::cout<<"x máximo: "<< x_max <<" ; x mínimo: "<< x_min << '\n';
     std::cout<<"y máximo: "<< y_max <<" ; y mínimo: "<< y_min << '\n';
     std::cout<<"z máximo: "<< z_max <<" ; z mínimo: "<< z_min << '\n';
 
-		Total_de_Pontos = currentLine;
 
     std::cout<<"Quantidade total de pontos: "<< Total_de_Pontos << '\n';
 
@@ -129,12 +115,12 @@ int main (int argc, char* argv[40])
     float chart_depth = (z_max - z_min)/ ((float) number_of_charts); //futuramente considerar fatiamento ao longo de outros eixos de acordo com as proporções do sólido
     std::cout << "Espessura da carta ao longo de z: " << chart_depth << '\n';
 
+		//criando arrays sob medida para armazenar pontos do arquivo:
 		float* x = (float*) malloc(sizeof(float)*Total_de_Pontos);
 		float* y = (float*) malloc(sizeof(float)*Total_de_Pontos);
 		float* z = (float*) malloc(sizeof(float)*Total_de_Pontos);
 
-		//fclose(file);
-		//file = fopen(filename, "r"); //, stdin);
+		//Armazenando pontos do arquivo em arrays sob medida:
 		rewind(file);
 
 			for (size_t i = 0; i < Total_de_Pontos; i++)
@@ -152,13 +138,14 @@ int main (int argc, char* argv[40])
 		}
 		*/
 
-		std::cout << "Cópia dos pontos concluída com sucesso." << '\n';
+		std::cout << "Cópia dos pontos concluída com sucesso." << '\n'<< '\n';
 
-		float* Atlas = (float*) malloc (sizeof(float)*number_of_charts*Points_per_Chart*3);
+		float* Atlas = (float*) malloc (sizeof(float)*Total_de_Pontos*3);
 
 		int Index_Vector[number_of_charts]; //conta o número de pontos em cada carta
 		bool* Oversized_Charts = (bool*) malloc(sizeof(bool)*number_of_charts); //marcador lógico para as cartas cujo número de pontos excedeu o máximo admitido
 																																						//deve ser usado para prosseguir com cortes da região coberta pela carta em outros planos
+		//inicializando vetores Index_Vector e Oversized_Charts:
 		for (size_t i = 0; i < number_of_charts; i++)
 		{
 			Index_Vector[i]=0;
@@ -167,33 +154,63 @@ int main (int argc, char* argv[40])
 
 		std::cout << "Memória para as cartas alocada com sucesso." << '\n';
 
+		//Contando os pontos de cada carta:
 		for (size_t i = 0; i < Total_de_Pontos; i++)
 		{
 			for (size_t chart_index = 0; chart_index < number_of_charts; chart_index++)
 			{
 				if ( (chart_index != number_of_charts -1) && (z[i]<=(z_max - chart_index*chart_depth)) && z[i]>(z_max - (chart_index + 1)*chart_depth) )
 				{
-					if (Index_Vector[chart_index]<Points_per_Chart)
-					{
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 0), x+i, sizeof(float));
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 1), y+i, sizeof(float));
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 2), z+i, sizeof(float));
-					}
 					Index_Vector[chart_index]++;
 				}
 				//making sure the last chart will take points at the inferior border:
 				if ((chart_index == number_of_charts -1) && (z[i]<=(z_max - chart_index*chart_depth)) && z[i]>=(z_min) )
 				{
-					if (Index_Vector[chart_index]<Points_per_Chart)
-					{
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 0), x+i, sizeof(float));
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 1), y+i, sizeof(float));
-						memcpy((Atlas + chart_index*Points_per_Chart*3 + 3*Index_Vector[chart_index] + 2), z+i, sizeof(float));
-					}
 					Index_Vector[chart_index]++;
 				}
 			}
 		}
+
+		std::cout << "Contagem de pontos em cada carta realizada com sucesso." << '\n';
+
+		//passando o conteúdo do vetor de índices para Qtd_pts_na_Carta
+		// e reinicializando-o para orientar o armazenamento dos pontos na carta:
+		int Qtd_pts_na_Carta[number_of_charts];
+		for (size_t i = 0; i < number_of_charts; i++)
+		{
+			Qtd_pts_na_Carta[i] = Index_Vector[i];
+			Index_Vector[i]=0;
+		}
+
+		//alocando pontos nas cartas: ---> O Atlas é uma coleção de matrizes (cartas), todas com 3 colunas
+		//-------------------------------> mas cada uma com uma quantidade de linhas diferente
+		//-------------------------------> a quantidade de linhas em cada carta é guardada no 'Qtd_pts_na_Carta';
+		//-------------------------------> o elemento que está na linha i e na coluna j da k-ésima carta se localiza na seguinte posição de memória:
+		//-------------------------------> (Atlas + 3*sum_sizes(k, Qtd_pts_na_Carta) + 3*i + j)
+		for (size_t i = 0; i < Total_de_Pontos; i++)
+		{
+			for (size_t chart_index = 0; chart_index < number_of_charts; chart_index++)
+			{
+				if ( (chart_index != number_of_charts -1) && (z[i]<=(z_max - chart_index*chart_depth)) && z[i]>(z_max - (chart_index + 1)*chart_depth) )
+				{
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 0), x+i, sizeof(float));
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 1), y+i, sizeof(float));
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 2), z+i, sizeof(float));
+					Index_Vector[chart_index]++;
+				}
+				//making sure the last chart will take points at the inferior border:
+				if ((chart_index == number_of_charts -1) && (z[i]<=(z_max - chart_index*chart_depth)) && z[i]>=(z_min) )
+				{
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 0), x+i, sizeof(float));
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 1), y+i, sizeof(float));
+					memcpy((Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*Index_Vector[chart_index] + 2), z+i, sizeof(float));
+					Index_Vector[chart_index]++;
+				}
+			}
+		}
+
+		std::cout << "Registro de pontos nas cartas realizado com sucesso." << '\n';
+
 
 		//marcando quais são as cartas que excederam o limite de pontos:
 		for (size_t chart_index = 0; chart_index < number_of_charts; chart_index++)
@@ -207,25 +224,27 @@ int main (int argc, char* argv[40])
 
     std::cout << "Particionamento dos pontos concluído com sucesso." << '\n' << '\n';
 
+
 		//Mostrando conteúdo das cartas no console:
-		int this_chart_in_particular = 0;
+		int this_chart_in_particular = 1;
 		bool show_points = false;
 		for (size_t chart_index = 0; chart_index < number_of_charts; chart_index++)
     {
 			if (show_points && (chart_index ==this_chart_in_particular))//mostra pontos da carta de número 'this_chart_in_particular' se show_points for true
 			{
 	      std::cout << "Pontos da carta " << chart_index << ": " << '\n';
-	      for (size_t j = 0; j < Index_Vector[chart_index]; j++)
+	      for (size_t j = 0; j < Qtd_pts_na_Carta[chart_index]; j++)
 	      {
-	        std::cout << "x = " << *(Atlas + chart_index*Points_per_Chart*3 + 3*j + 0) <<
-					" ; y = " << *(Atlas + chart_index*Points_per_Chart*3 + 3*j + 1) <<
-					" ; z = " << *(Atlas + chart_index*Points_per_Chart*3 + 3*j + 2) <<'\n';
+	        std::cout << "x = " << *(Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*j + 0) <<
+					" ; y = " << *(Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*j + 1) <<
+					" ; z = " << *(Atlas + 3*sum_sizes(chart_index, Qtd_pts_na_Carta) + 3*j + 2) <<'\n';
 	      }
 			}
-			std::cout << "Quantidade de pontos na carta " << chart_index << ": "<< Index_Vector[chart_index] <<
+			std::cout << "Quantidade de pontos na carta " << chart_index << ": "<< Qtd_pts_na_Carta[chart_index] <<
 			" ;    Dimensões:  z max = " << (z_max - chart_index*chart_depth) << " ; z min = " << (z_max - (chart_index + 1)*chart_depth) << '\n';
 		}
 
+		//registrando o excesso de pontos nas cartas:
 		for (size_t i = 0; i < number_of_charts; i++)
 		{
 			if (Index_Vector[i] >= Points_per_Chart)
@@ -234,15 +253,33 @@ int main (int argc, char* argv[40])
 			}
 		}
 
-		{
-		int sum;
+
 		sum=0;
 		for (size_t i = 0; i < number_of_charts; i++) {sum = sum + Index_Vector[i];}
 		std::cout << "\nTotal de pontos copiados para as cartas: " << sum <<'\n';
-		}
 
-		std::cout << "Iniciando sub-fatiamento:" << '\n';
 
+		std::cout << "Iniciando sub-fatiamento." << '\n';
+
+		//tomando dimensões das regiões que excederam o limite de pontos:
+		std::cout << "Iniciando medidas de densidade linear de pontos ao longo dos eixos coordenados." << '\n';
+
+
+		//int* X_Linear_Points_density = (int*) malloc(    );
+		//int* Y_Linear_Points_density = (int*) malloc(    );
+		//int* Z_Linear_Points_density = (int*) malloc(    );
+
+
+
+
+
+
+
+
+
+		//free(X_Linear_Points_density);
+		//free(Y_Linear_Points_density);
+		//free(Z_Linear_Points_density);
 		free(Oversized_Charts);
 		free(Atlas);
 		free(z);
